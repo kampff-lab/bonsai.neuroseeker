efa3cc21762d60788da8ea5841fcf97e42d278c2
glopes <glopes@localhost>
2016-11-23 11:09:07 +0000

    Added support for File to read synchronization data.

--- Bonsai.NeuroSeeker/File.cs
+++ Bonsai.NeuroSeeker/File.cs
@@ -12,14 +12,14 @@ using System.Runtime.InteropServices;
 using OpenCV.Net;
 
 // TODO: replace this with the source output type.
-using TSource = OpenCV.Net.Mat;
+using TSource = Bonsai.NeuroSeeker.NskDataFrame;
 
 namespace Bonsai.NeuroSeeker
 {
     public class File : Source<TSource>
     {
         // Class variables
-        IObservable<OpenCV.Net.Mat> source;
+        IObservable<NskDataFrame> source;
         private int n_channels = 1440;
 
         // Properties
@@ -42,13 +42,14 @@ namespace Bonsai.NeuroSeeker
 
         // Import relevant functions from Nsk C DLL
         [DllImport("NeuroSeeker_C_DLL", CallingConvention = CallingConvention.Cdecl)]
-        private static extern int NSK_Read_File(IntPtr buffer, int buffer_size);
-        public static OpenCV.Net.Mat NSK_Read_File(int n_channels, int buffer_size)
+        private static extern int NSK_Read_File(IntPtr buffer, IntPtr sync, int buffer_size);
+        public static NskDataFrame NSK_Read_File(int n_channels, int buffer_size)
         {
             var result = new OpenCV.Net.Mat(n_channels, buffer_size, OpenCV.Net.Depth.F32, 1);
-            var samplesRead = NSK_Read_File(result.Data, buffer_size);
+            var sync = new OpenCV.Net.Mat(1, buffer_size, OpenCV.Net.Depth.U16, 1);
+            var samplesRead = NSK_Read_File(result.Data, sync.Data, buffer_size);
             if (samplesRead == 0) return null;
-            return result;
+            return new NskDataFrame(result, sync);
         }
 
         // Import relevant functions from Nsk C DLL
@@ -62,7 +63,7 @@ namespace Bonsai.NeuroSeeker
             BufferSize = 500;
 
             // Create a source of CvMats
-            source = Observable.Create<Mat>((observer, cancellationToken) =>
+            source = Observable.Create<NskDataFrame>((observer, cancellationToken) =>
             {
                 return Task.Factory.StartNew(() =>
                 {
